6b592d9e1f2add45f89a0c47f2420f48c74f3f04
sekhar-01 <sekharalusuri@gmail.com>
2022-05-18 11:55:39 +0530

    head tag added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { useState } from 'react';
+
+
+function App(){
+const [count,setcount]=useState(0)
+    return(
+      <div>
+      <p>You clicked {count} times</p>
+      <h1>hello</h1>
+      <button onClick={()=>setcount(count+1)}>Click</button>
+      </div>
+    );
+  
 }
 
 export default App;
+
